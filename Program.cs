2af118910903f1cdac63c4923c651a54a5407605
Carter S <speers.carter@gmail.com>
2024-05-31 19:57:42 -0400

    Final Commit
    finished with a working solution that passes all unit tests.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+namespace exercise_2_2;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+
+    public static void Main(String[] args)
+    {
+        const int baseCost = 200;
+        const int hourlyRate = 150;
+        const int mileRate = 2;
+
+        Console.WriteLine("how many hours will the move take?");
+        var hours = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("how many miles is the move");
+        var miles = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Total: {0:C}", hours*hourlyRate + mileRate * miles + baseCost);
+    }
+}
